72a477ccbd390f61b7d2fbf0b6486a2f70931d48
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2025-08-09 14:03:13 +0000

    Convert from express to slim

--- src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/user.controller');
-const { verifyToken } = require('../middlewares/auth.middleware');
-
-router.get('/', verifyToken, userController.getAllUsers);
-router.post('/', userController.createUser);
-
-module.exports = router;
